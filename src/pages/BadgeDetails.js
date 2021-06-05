c37205b7529b091ad863fa7cb63208d33cbd803b
gio <giojimarc@gmail.com>
2021-06-05 10:33:17 -0500

    Finalizacion platzi badges

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -2,95 +2,90 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./styles/BadgeDetails.css";
-import confLogo from "../images/platziconf-logo.svg";
-import PageLoading from "../components/PageLoading";
-import PageError from "../components/PageError";
+import confLogo, { ReactComponent } from "../images/platziconf-logo.svg";
 import Badge from "../components/Badge";
-import api from "../api";
+import DeleteBadgeModal from "../components/DeleteBadgeModal";
 
-class BadgeDetails extends React.Component {
-  state = {
-    loading: true,
-    error: null,
-    data: undefined,
-  };
+function useIncreaseCount(max) {
+  const [count, setCount] = React.useState(0);
 
-  componentDidMount() {
-    this.fetchData();
+  if (count > max) {
+    setCount(0);
   }
 
-  fetchData = async () => {
-    this.setState({ loading: true, error: null });
+  return [count, setCount];
+}
 
-    try {
-      const data = await api.badges.read(this.props.match.params.badgeId);
-      this.setState({ loading: false, data: data });
-    } catch (error) {
-      this.setState({ loading: false, error: error });
-    }
-  };
+function BadgeDetails(props) {
+  const [count, setCount] = useIncreaseCount(4);
 
-  render() {
-    if (this.state.loading) {
-      return <PageLoading />;
-    }
+  const badge = props.badge;
 
-    if (this.state.error) {
-      return <PageError error={this.state.error} />;
-    }
-
-    const badge = this.state.data;
-
-    return (
-      <div>
-        <div className="BadgeDetails__hero">
-          <div className="container">
-            <div className="row">
-              <div className="col-6">
-                <img src={confLogo} alt="Logo de la Conferencia" />
-              </div>
-              <div className="col-6 BadgeDetails__hero-attendant-name">
-                <h1>
-                  {badge.firstName} {badge.lastName}
-                </h1>
-              </div>
+  return (
+    <React.Fragment>
+      <div className="BadgeDetails__hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <img src={confLogo} alt="Logo de la Conferencia" />
+            </div>
+            <div className="col-6 BadgeDetails__hero-attendant-name">
+              <h1>
+                {badge.firstName} {badge.lastName}
+              </h1>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <Badge
-                firstName={badge.firstName}
-                lastName={badge.lastName}
-                email={badge.email}
-                twitter={badge.twitter}
-                jobTitle={badge.jobTitle}
-              />
-            </div>
-            <div className="col">
-              <h2>Actions</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Badge
+              firstName={badge.firstName}
+              lastName={badge.lastName}
+              email={badge.email}
+              twitter={badge.twitter}
+              jobTitle={badge.jobTitle}
+            />
+          </div>
+          <div className="col">
+            <h2>Actions</h2>
+            <div>
               <div>
-                <div>
-                  <Link
-                    className="btn btn-primary mb-4"
-                    to={`/badges/${badge.id}/edit`}
-                  >
-                    Edit
-                  </Link>
-                </div>
+                <button
+                  onClick={() => {
+                    setCount(count + 1);
+                  }}
+                  className="btn btn-primary mr-4"
+                >
+                  Increase Count: {count}
+                </button>
 
-                <div>
-                  <button className="btn btn-danger">Delete</button>
-                </div>
+                <Link
+                  className="btn btn-primary mb-4"
+                  to={`/badges/${badge.id}/edit`}
+                >
+                  Edit
+                </Link>
+              </div>
+
+              <div>
+                <button onClick={props.onOpenModal} className="btn btn-danger">
+                  Delete
+                </button>
+                <DeleteBadgeModal
+                  isOpen={props.modalIsOpen}
+                  onClose={props.onCloseModal}
+                  onDeleteBadge={props.onDeleteBadge}
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </React.Fragment>
+  );
 }
 
 export default BadgeDetails;
